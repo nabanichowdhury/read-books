d6e78c0a82d9990dbd81683a1aa513526b00080b
nabanichowdhury <nabanichowdhury@gmail.com>
2022-04-05 17:42:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import DashBoard from './Components/DashBoard/DashBoard';
+import Home from './Components/Home/Home';
+import Notfound from './Components/NotFound/Notfound';
+import Review from './Components/Review/Review';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/reviews" element={<Review/>}></Route>
+        <Route path="/dashboard" element={<DashBoard/>}></Route>
+        <Route path="*" element={<Notfound></Notfound>}></Route>
+      </Routes>
+      
+      
     </div>
   );
 }
---
+++ src/Components/NotFound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h1>The page you are looking about is not available</h1>
+            <h3>404</h3>
+        </div>
+    );
+};
+
+export default Notfound;
