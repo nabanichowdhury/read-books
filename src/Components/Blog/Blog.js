1526fce8866c7195e1412fc2316986ed332a97eb
nabanichowdhury <nabanichowdhury@gmail.com>
2022-04-28 03:40:12 +0600

    did-css

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,10 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <h1>blog writing</h1>
+      <h1>Q1.What is Simantic Tag ?</h1>
+      <p>Ans:</p>
+      <h1>Q2.What is inline block element?</h1>
+      <p></p>
     </div>
   );
 };
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,3 +1,5 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Review.css";
 
@@ -7,6 +9,14 @@ const Review = (props) => {
     <div className="review">
       <h3>Name:{name}</h3>
       <p>{review}</p>
+      <div>
+        <p>Ratings:5 stars</p>
+        <FontAwesomeIcon className="star" icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className="star" icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className="star" icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className="star" icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className="star" icon={faStar}></FontAwesomeIcon>
+      </div>
     </div>
   );
 };
